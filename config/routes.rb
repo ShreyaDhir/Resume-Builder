c9f0899e5e77276b99ffb72b09e6a2ae4a10bdf4
ShreyaDhir <dhirshreya@gmail.com>
2022-03-09 22:02:00 -0800

    update files

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get    'edit'    => 'home#edit'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
+  get    'sign_up'  => 'registrations#new'
   delete 'logout'  => 'sessions#destroy'
-
   resources :profiles , only: [:update]
   resources :educations , only: [:new]
 end
